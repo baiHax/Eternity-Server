748293f0f3a5b9991009b833c5a52966fff02f38
Fenriis <danielperezgarcia290290@gmail.com>
2016-04-27 20:00:12 +0000

    agrego pmll

---
+++ chat-plugins/eternity.js
@@ -0,0 +1,57 @@
+exports.commands = {
+	/*stafflist: 'authlist',
+	authlist: function (target, room, user, connection) {
+		var rankLists = {};
+		for (var u in Users.usergroups) {
+			var rank = Users.usergroups[u][0];
+			var name = Users.usergroups[u].slice(1);
+			if (!rankLists[rank]) rankLists[rank] = [];
+			if (name) name = name.replace("\n", "").replace("\r", "");
+			rankLists[rank].push(name);
+		}
+		var buffer = [];
+		Object.keys(rankLists).sort(function (a, b) {
+			return Config.groups[b].rank - Config.groups[a].rank;
+		}).forEach(function (r) {
+			buffer.push(Config.groups[r].name + "s (" + r + "):\n" + rankLists[r].sort().join(", "));
+		});
+
+		if (!buffer.length) {
+			buffer = "This server has no auth.";
+			return connection.popup("This server has no auth.");
+		}
+		connection.popup(buffer.join("\n\n"));
+	},*/
+	
+	masspm: 'pmall',
+	pmall: function (target, room, user) {
+		if (!this.can('pmall')) return false;
+		if (!target) return this.parse('/help pmall');
+
+		var pmName = ' Sora-Info [No Responder]';
+
+		for (var i in Users.users) {
+			var message = '|pm|' + pmName + '|' + Users.users[i].getIdentity() + '|' + target;
+			Users.users[i].send(message);
+		}
+	},
+	pmallhelp: ["/pmall [message] - Envia un MP a todos en el Server"],
+
+	staffpm: 'pmallstaff',
+	pmstaff: 'pmallstaff',
+	pmallstaff: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target) return this.parse('/help pmallstaff');
+
+		var pmName = ' Staff-Info [No Responder]';
+
+		for (var i in Users.users) {
+			if (Users.users[i].isStaff) {
+				Users.users[i].send('|pm|' + pmName + '|' + Users.users[i].group + Users.users[i].name + '|' + target);
+			}
+		}
+	},
+	
+ 
+};
+	
